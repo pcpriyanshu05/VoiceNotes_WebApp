a282d378945a354e2b4b1309ad5ec9063c992acb
Priyanshu <pcauthentic123@gmail.com>
2026-01-31 12:59:52 +0530

    Updated

--- routes/whisperRoutes.js
+++ routes/whisperRoutes.js
@@ -1,8 +1,8 @@
-const express = require("express");
-const router = express.Router();
-const upload = require("../config/multer");
-const { transcribeAudio } = require("../controllers/whisperController");
+// const express = require("express");
+// const router = express.Router();
+// const upload = require("../config/multer");
+// const { transcribeAudio } = require("../controllers/whisperController");
 
-router.post("/", upload.single("audio"), transcribeAudio);
+// router.post("/", upload.single("audio"), transcribeAudio);
 
-module.exports = router;
+// module.exports = router;
